b570d2860df8707f35721a491df6e34ac91ac7a2
satyvarthrathi <138241148+satyvarthrathi@users.noreply.github.com>
2023-08-24 22:02:40 +0530

    Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract EthereumATM {
+contract Assessment {
     address public owner;
     mapping(address => uint256) public balances;
 
