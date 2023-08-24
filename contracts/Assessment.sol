18cd52ba9ea2920af2412af8b75d2784980ff458
satyvarthrathi <138241148+satyvarthrathi@users.noreply.github.com>
2023-08-24 15:05:08 +0530

    Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,53 +1,47 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
+contract EthereumATM {
+    address public owner;
+    mapping(address => uint256) public balances;
 
-contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    event Deposit(address indexed account, uint256 amount);
+    event Withdrawal(address indexed account, uint256 amount);
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    constructor() {
+        owner = msg.sender;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        
-        balance += _amount;
-        assert(balance == _previousBalance + _amount);
-        emit Deposit(_amount);
+    function deposit() external payable {
+        require(msg.value > 0, "Amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
 
-    function withdraw(uint256 _withdrawAmount) public {
-        // require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdrawal(msg.sender, amount);
+    }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
+    function getBalance() external view returns (uint256) {
+        return balances[msg.sender];
+    }
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+    function contractBalance() external view onlyOwner returns (uint256) {
+        return address(this).balance;
+    }
 
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    // Owner can withdraw remaining balance from the contract
+    function withdrawContractBalance() external onlyOwner {
+        payable(owner).transfer(address(this).balance);
     }
 }
