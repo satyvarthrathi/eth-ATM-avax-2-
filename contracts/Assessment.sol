b857836896d881d33f5a18d5cc89ff362cd28c53
satyvarthrathi <138241148+satyvarthrathi@users.noreply.github.com>
2023-09-23 21:25:43 +0530

    Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,47 +1,35 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Assessment {
+contract Assesment {
+    uint public unlockTime;
     address public owner;
-    mapping(address => uint256) public balances;
+    uint public lockedAmount;
 
-    event Deposit(address indexed account, uint256 amount);
-    event Withdrawal(address indexed account, uint256 amount);
+    event Withdrawal(address indexed owner, uint amount);
+
+    constructor(uint _unlockTime) payable {
+        require(_unlockTime > block.timestamp, "Unlock time should be in the future");
+
+        unlockTime = _unlockTime;
+        owner = msg.sender;
+        lockedAmount = msg.value;
+    }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
+        require(msg.sender == owner, "You aren't the owner");
         _;
     }
 
-    constructor() {
-        owner = msg.sender;
-    }
+    function withdraw() public onlyOwner {
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
 
-    function deposit() external payable {
-        require(msg.value > 0, "Amount must be greater than 0");
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
+        uint amount = lockedAmount;
+        lockedAmount = 0;
 
-    function withdraw(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(balances[msg.sender] >= amount, "Insufficient balance");
+        emit Withdrawal(owner, amount);
 
-        balances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
-        emit Withdrawal(msg.sender, amount);
-    }
-
-    function getBalance() external view returns (uint256) {
-        return balances[msg.sender];
-    }
-
-    function contractBalance() external view onlyOwner returns (uint256) {
-        return address(this).balance;
-    }
-
-    // Owner can withdraw remaining balance from the contract
-    function withdrawContractBalance() external onlyOwner {
-        payable(owner).transfer(address(this).balance);
+        (bool success, ) = owner.call{value: amount}("");
+        require(success, "Transfer failed");
     }
 }
